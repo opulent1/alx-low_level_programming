f020a7e0641b8000a7819b040fcfcfb4fb2dbaf3
opulent1 <emmzzy68@gmail.com>
2022-10-16 04:54:07 -0700

    my second c priogram

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stuio.h>
+/**
+ * main -print code using printf
+ *
+ * return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art," \n);
+	return (0);
+}
