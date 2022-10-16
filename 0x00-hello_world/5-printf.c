eb6be73022c8c7f0456b54f530e197ce55d4375f
opulent1 <emmzzy68@gmail.com>
2022-10-16 05:00:34 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main -print code using printf
+ * main - print code using printf
  *
- * return: 0
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art," \n);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
